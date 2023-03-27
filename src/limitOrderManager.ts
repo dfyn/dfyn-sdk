f76f2d91d42cfe56020bea13561816d306ba39db
Joydeep Singha <joydeepsingha68@gmail.com>
2023-03-27 11:34:27 +0530

    fix: typo fix

--- src/limitOrderManager.ts
+++ src/limitOrderManager.ts
@@ -1,111 +1,112 @@
 import { Interface } from "@ethersproject/abi";
 import { Order } from "entities/order";
-import { MethodParameters, Percent,Currency, Pool, toHex, wrappedCurrency } from "./index";
+import { MethodParameters, Percent, Currency, Pool, toHex, wrappedCurrency } from "./index";
 import invariant from "tiny-invariant";
 import LimitOrderManagerAbi from "./abis/LimitOrderManager.json"
 import JSBI from 'jsbi'
 import { BigintIsh, ZERO } from "./constants";
+import { Multicall } from "./multicall";
+
+/**
+ * Options for producing the calldata to create limit order.
+ */
+export interface CreateOrderOptions {
+  /**
+   * How much the pool price is allowed to move.
+   */
+  slippageTolerance: Percent
 
   /**
-   * Options for producing the calldata to create limit order.
+   * When the transaction expires, in epoch seconds.
    */
-export interface CreateOrderOptions{
-    /**
-     * How much the pool price is allowed to move.
-     */
-     slippageTolerance: Percent
-  
-     /**
-      * When the transaction expires, in epoch seconds.
-      */
-     deadline: BigintIsh
-   
-     /**
-      * Whether to spend ether. If true, one of the pool tokens must be WETH, by default false
-      */
-     useNative?: Currency
+  deadline: BigintIsh
 
-     /**
-     * The lower and upper old
-     */
-     lowerOldTick: number
+  /**
+   * Whether to spend ether. If true, one of the pool tokens must be WETH, by default false
+   */
+  useNative?: Currency
 
-     /**
-     * The lower and upper old
-     */
-     upperOldTick: number
+  /**
+  * The lower and upper old
+  */
+  lowerOldTick: number
 
-     /**
-     * The lower and upper old
-     */
-     recipient: string
- 
-}
+  /**
+  * The lower and upper old
+  */
+  upperOldTick: number
 
-export interface ClaimOrderOptions{
-    /**
-     * Indicates the ID of the position to collect for.
-     */
-    tokenId: BigintIsh
-
-     /**
-      * When the transaction expires, in epoch seconds.
-      */
-     deadline: BigintIsh
-
-     unwrapVault: Boolean
+  /**
+  * The lower and upper old
+  */
+  recipient: string
 
 }
-export interface CancelOrderOptions{
-    /**
-     * Indicates the ID of the position to collect for.
-     */
-    tokenId: BigintIsh
 
-     /**
-      * When the transaction expires, in epoch seconds.
-      */
-     deadline: BigintIsh
+export interface ClaimOrderOptions {
+  /**
+   * Indicates the ID of the position to collect for.
+   */
+  tokenId: BigintIsh
 
-     unwrapVault: Boolean
+  /**
+   * When the transaction expires, in epoch seconds.
+   */
+  deadline: BigintIsh
+
+  unwrapVault: Boolean
+
+}
+export interface CancelOrderOptions {
+  /**
+   * Indicates the ID of the position to collect for.
+   */
+  tokenId: BigintIsh
+
+  /**
+   * When the transaction expires, in epoch seconds.
+   */
+  deadline: BigintIsh
+
+  unwrapVault: Boolean
 
 }
 
 
 
 export abstract class LimitOrderManager {
-    public static INTERFACE: Interface = new Interface(LimitOrderManagerAbi);
+  public static INTERFACE: Interface = new Interface(LimitOrderManagerAbi);
 
-    /**
-   * Cannot be constructed.
-   */
-  private constructor() {}
+  /**
+ * Cannot be constructed.
+ */
+  private constructor() { }
 
-  public static createCallParameters(order:Order, options:CreateOrderOptions):MethodParameters {
+  public static createCallParameters(order: Order, options: CreateOrderOptions): MethodParameters {
     invariant(JSBI.greaterThan(order.tokenAmountIn.raw, ZERO), 'ZERO_AMOUNT_IN')
 
     let calldata: string
 
     // get amounts
-    const {zeroForOne,tokenAmountIn,tick}=order;
-    const {lowerOldTick,upperOldTick}=options;
-    const amountIn=tokenAmountIn.raw;
-    const recipient=options.recipient;
+    const { zeroForOne, tokenAmountIn, tick } = order;
+    const { lowerOldTick, upperOldTick } = options;
+    const amountIn = tokenAmountIn.raw;
+    const recipient = options.recipient;
 
-    calldata=this.INTERFACE.encodeFunctionData('createLimitOrder',[
-        Pool.getAddress(order.pool.token0,order.pool.token1),
-        tick,
-        lowerOldTick,
-        upperOldTick,
-        true,
-        amountIn.toString(),
-        zeroForOne,
-        recipient
+    calldata = this.INTERFACE.encodeFunctionData('createLimitOrder', [
+      Pool.getAddress(order.pool.token0, order.pool.token1),
+      tick,
+      lowerOldTick,
+      upperOldTick,
+      true,
+      amountIn.toString(),
+      zeroForOne,
+      recipient
     ])
 
     let value: string = toHex(0)
     if (options.useNative) {
-      const wrapped = wrappedCurrency(options.useNative,order.tokenAmountIn.token.chainId)
+      const wrapped = wrappedCurrency(options.useNative, order.tokenAmountIn.token.chainId)
       invariant(order.tokenAmountIn.token.equals(wrapped), 'NO_WETH')
 
       const wrappedValue = amountIn.toString()
@@ -114,53 +115,53 @@ export abstract class LimitOrderManager {
     }
 
     return {
-        calldata,
-        value
-      }
+      calldata,
+      value
+    }
 
   }
 
-  public static claimCallParameters(options:ClaimOrderOptions):MethodParameters {
+  public static claimCallParameters(options: ClaimOrderOptions): MethodParameters {
     let calldata
 
     const tokenId = toHex(options.tokenId)
-    const unwrapVault=options.unwrapVault
+    const unwrapVault = options.unwrapVault
     // const involvesETH =
     //   false
 
-    calldata=this.INTERFACE.encodeFunctionData('claimLimitOrder',[
-        tokenId,
-        unwrapVault
+    calldata = this.INTERFACE.encodeFunctionData('claimLimitOrder', [
+      tokenId,
+      unwrapVault
     ])
 
-    const  value = toHex(0)
+    const value = toHex(0)
 
     return {
-        calldata,
-        value
-      }
+      calldata,
+      value
+    }
 
 
   }
-  public static cancelallParameters(options:CancelOrderOptions):MethodParameters {
+  public static cancelCallParameters(options: CancelOrderOptions): MethodParameters {
     let calldata
 
     const tokenId = toHex(options.tokenId)
-    const unwrapVault=options.unwrapVault
+    const unwrapVault = options.unwrapVault
     // const involvesETH =
     //   false
 
-    calldata=this.INTERFACE.encodeFunctionData('cancelLimitOrder',[
-        tokenId,
-        unwrapVault
+    calldata = this.INTERFACE.encodeFunctionData('cancelLimitOrder', [
+      tokenId,
+      unwrapVault
     ])
 
-    const  value = toHex(0)
+    const value = toHex(0)
 
     return {
-        calldata,
-        value
-      }
+      calldata,
+      value
+    }
 
   }
 
